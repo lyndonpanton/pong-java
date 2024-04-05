f80f752c303960ed9476dcc4480d65b6a9742e34
Lyndon Panton <LyndonPanton@gmail.com>
2024-04-05 23:01:58 +0100

    Defined triggers for the key presses and releases

---
+++ src/game/Action.java
@@ -0,0 +1,16 @@
+package game;
+
+public class Action {
+    /*
+        thrust off: 0
+        thrust on: 1
+     */
+    public int thrust;
+    /*
+        left: -1
+        none: 0
+        right: `
+     */
+    public int turn;
+    public boolean isShooting;
+}
