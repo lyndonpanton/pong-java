81ebbbf76060975cdc48431d5468cc5e42eb484d
Lyndon Panton <LyndonPanton@gmail.com>
2024-04-05 23:17:19 +0100

    Defined key press and key release triggers

---
+++ src/utilities/Keys.java
@@ -0,0 +1,48 @@
+package utilities;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class Keys extends KeyAdapter implements Controller {
+    private Action action;
+
+    public Keys()
+    {
+        action = new Action();
+    }
+
+    public Action getAction() {
+        // Defined to be inline with the interface definition
+        return action;
+    }
+
+    public void keyPressed(KeyEvent e) {
+        int key = e.getKeyCode();
+
+        switch (key) {
+            case KeyEvent.VK_W:
+            case KeyEvent.VK_UP:
+                action.movement = -1;
+                break;
+            case KeyEvent.VK_S:
+            case KeyEvent.VK_DOWN:
+                action.movement = 1;
+                break;
+            case KeyEvent.VK_ESCAPE:
+                action.isPaused = !action.isPaused;
+        }
+    }
+
+    public void keyReleased(KeyEvent e) {
+        int key = e.getKeyCode();
+
+        switch (key) {
+            case KeyEvent.VK_W:
+            case KeyEvent.VK_UP:
+            case KeyEvent.VK_S:
+            case KeyEvent.VK_DOWN:
+                action.movement = 0;
+                break;
+        }
+    }
+}
